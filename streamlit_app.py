227f82aa7761868707b2be696b0026a1167e490f
Arvind Krishna <49233881+Arvind-krishna@users.noreply.github.com>
2023-10-31 14:55:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,21 +15,36 @@ st.write('''It's like teaching a computer to recognize your writing, just like y
 st.write('''Think of a neural network as a super-smart detective who learns by looking at lots of different handwriting examples. ''')
 st.write('''This detective then figures out which letters and words are written in a special code. Once the detective knows the code, it can read what you wrote and even tell you what it says!''')
 
-st.subheader('''Step 1 : Training our Handwriting detective''')
+st.subheader('''Step 1 : Gathering evidence''')
 
-st.write('''"Before our detective (the neural network) can become really good at reading handwriting, it needs some training, just like when you learn to ride a bicycle. ''')
-st.write('''We show it lots and lots of different handwritten examples of the numbers from 0-9 This will be our dataset. It practices and practices until it gets better at recognizing them. ''')
-st.write('''This part is like the detective's training.''')
+st.write('''"Before our detective (the neural network) can become really good at reading handwriting, it needs some high quality evidence/clues, and also some training, just like when you learn to ride a bicycle. ''')
 
-st.write('''Here's a small activity. Pick a color from below, and observe the numbers.''')
-selected_color = st.color_picker("Pick a color", "#FF5733")
-st.subheader(f'<span style="color: {selected_color};">0123456789</span>', unsafe_allow_html=True)
+
 
 code='''from sklearn.datasets import fetch_openml 
 mnist = fetch_openml('mnist_784', as_frame=False, cache=False) #Loads mnist dataset from sklearn'''
 
 st.code(code, language='python')
 
-st.write('''Awesome!. We've now loaded the dataset which ''')
+st.write('''Awesome!. We've now loaded the dataset of handwritten numbers from 0-9. That's a great start!. now, to train our detective.''')
+
+question = "Ok, What should we do next?"
+choices = ["Train the detective!", "Test the detective!", "Train, and then test the detective!"]
+correct_answer = "Train, and then test the detective"
+
+# Create a multiple-choice question using radio buttons
+user_answer = st.radio("Select your answer:", choices)
+
+# Check if the user's answer is correct and display feedback
+if user_answer == correct_answer:
+    st.markdown("**Correct!** :green_heart:")
+else:
+    st.markdown("**Not quite.** :orange_heart:")
+
+
+st.subheader('''Step 1 : Gathering evidence''')
+
+st.write('''We show it lots and lots of different handwritten examples of the numbers from 0-9 This will be our dataset. It practices and practices until it gets better at recognizing them. ''')
+st.write('''This part is like the detective's training.''')
 
 
