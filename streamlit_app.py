d8cdf135450282324094083eb0456fb606f0f1fb
Arvind Krishna <49233881+Arvind-krishna@users.noreply.github.com>
2023-10-31 13:36:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 
 st.title("Vizuara AI Labs : Handwritten Text Classification")
-st.image("image.jpg")
+st.image("image.jpg", width=300)
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
