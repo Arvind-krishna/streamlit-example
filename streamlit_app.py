248d1a3a3eb451a702f2123725e910f132d7cdbe
Arvind Krishna <49233881+Arvind-krishna@users.noreply.github.com>
2023-11-10 18:38:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -269,29 +269,29 @@ if st.button('Train Model'):
     st.code(f"Accuracy score: {test_acc}")
     st.markdown(f"Wow, our neural network was able to predict with an accuracy of  {test_acc}! For a network with only one hidden layer, it is not too bad!")
     
-st.markdown('''Go ahead, try it out yourself.. Try to write a digit!''')
-SIZE = 192
-mode = st.checkbox("Draw (or Delete)?", True)
-canvas_result = st_canvas(
-fill_color='#000000',
-stroke_width=20,
-stroke_color='#FFFFFF',
-background_color='#000000',
-width=SIZE,
-height=SIZE,
-drawing_mode="freedraw" if mode else "transform",key='canvas')
+    st.markdown('''Go ahead, try it out yourself.. Try to write a digit!''')
+    SIZE = 192
+    mode = st.checkbox("Draw (or Delete)?", True)
+    canvas_result = st_canvas(
+    fill_color='#000000',
+    stroke_width=20,
+    stroke_color='#FFFFFF',
+    background_color='#000000',
+    width=SIZE,
+    height=SIZE,
+    drawing_mode="freedraw" if mode else "transform",key='canvas')
 
-if canvas_result.image_data is not None:
-    img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
-    rescaled = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
-    st.write('Model Input')
-    st.image(rescaled)
+    if canvas_result.image_data is not None: 
+        img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
+        rescaled = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
+        st.write('Model Input')
+        st.image(rescaled)
 
-if st.button('Predict'):
-    test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    val = model.predict(test_x.reshape(1, 28, 28))
-    st.write(f'result: {np.argmax(val[0])}')
-    st.bar_chart(val[0])
+    if st.button('Predict'):
+        test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        val = model.predict(test_x.reshape(1, 28, 28))
+        st.write(f'result: {np.argmax(val[0])}')
+        st.bar_chart(val[0])
 
 ##############################
 
